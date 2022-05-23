8cffded24f58a97d838f4666fde9fd0aec7027f1
Jeffrey Jiang <jjiang30@stuy.edu>
2022-05-23 11:47:39 -0400

    created and added Salesman class

--- Node.java
+++ Node.java
@@ -5,15 +5,18 @@ public class Node {
     private double x;
     private double y;
 
-    private Map<Node, Double> pheromoneMap;
+    public Node(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-    public Node() {
+    public void updatePheromones(Node adj) {
 
     }
 
-    public void updatePheromones(Node adj) {}
+    public void decayPheromone() {
 
-    public void decayPheromone() {}
+    }
 
     private double calcDecay(Node adj) {
         return 0.0;
---
+++ Salesman.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Salesman {
+    
+    public static final List<Node> nodes = new ArrayList<>();
+    public static final Map<Set<Node>, Double> pheromoneMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        
+    }
+
+}
