37fbb45a10476f4722f4f9badd0f884a1eaf6ab5
Aiden Mizhen <amizhen30@stuy.edu>
2022-05-23 11:46:25 -0400

    Created Template

--- Ant.java
+++ Ant.java
@@ -1,9 +1,30 @@
 import java.util.*
+
 public class Ant {
     private double distanceTraveled;
     public ArrayList<Node> nodesVisited;
-    public Ant(){
+
+    public Ant() {
         distanceTraveled = 0;
         nodesVisited = new ArrayList<Node>();
+    }
+
+    private double calcPheromones() {
+        return 0.0;
+    }
+
+    private void pickNextNode() {
+    }
+
+    public void run() {
+
+    }
+
+    public void depositPheromones() {
+
+    }
+
+    public getDistance() {
+        return distanceTraveled;
     }
 }
