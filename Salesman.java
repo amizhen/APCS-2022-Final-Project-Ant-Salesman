8f989e128bb418e42f4ea365ca7368b1fbe18b68
Aiden Mizhen <amizhen30@stuy.edu>
2022-05-23 16:14:14 -0400

    Started Salesman class

---
+++ Salesman.java
@@ -0,0 +1,18 @@
+import java.util.*
+public class Salesman {
+    public static ArrayList<Node> nodes;
+    public static HashMap<Node><HashMap> pheromones;
+
+    public static Ant findShortestPath(){
+        return null;
+    }
+
+    public static void main(String[] args){
+        nodes = new ArrayList<>();
+        pheromones = new HashMap();
+    }
+}
+
+
+// Started work on ant class but realised it would make more sense to have the constants in the main written first.
+
