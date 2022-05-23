e46ff940bcfd452218c063b6ba61462401834130
Aiden Mizhen <amizhen30@stuy.edu>
2022-05-23 11:40:37 -0400

    Got started

---
+++ Ant.java
@@ -0,0 +1,9 @@
+import java.util.*
+public class Ant {
+    private double distanceTraveled;
+    public ArrayList<Node> nodesVisited;
+    public Ant(){
+        distanceTraveled = 0;
+        nodesVisited = new ArrayList<Node>();
+    }
+}
