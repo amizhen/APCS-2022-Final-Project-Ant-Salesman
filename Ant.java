13f45e309e15473932a8626b228cb5e7cc041efb
Aiden Mizhen <amizhen30@stuy.edu>
2022-05-24 16:39:47 -0400

    M

--- Ant.java
+++ Ant.java
@@ -15,7 +15,7 @@ public class Ant {
     }
 
     private double calc_dist(double x1, double y1, double x2, double y2){
-        return Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2));
+        return Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2));
     }
 
     private void pickNextNode() {
