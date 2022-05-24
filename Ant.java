499563a4c882923e2681d3d030f83e443484c391
Aiden Mizhen <amizhen30@stuy.edu>
2022-05-24 11:36:36 -0400

    Added accessor for node, and utility function to ant

--- Ant.java
+++ Ant.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.*;
 
 public class Ant {
     private double distanceTraveled;
@@ -10,8 +10,10 @@ public class Ant {
         nodesVisited = new ArrayList<Node>();
     }
 
-    private double calcPheromones() {
-        return 0.0;
+    private double calcPheromones(Node node1, Node node2) {
+        double[] cords1 = node1.getCords();
+        double[] cords2 = node2.getCords();
+        return calcDistance(cords1[0], cords2[])
     }
 
     private void pickNextNode() {
@@ -27,7 +29,11 @@ public class Ant {
 
     }
 
-    public getDistance() {
+    private double calcDistance(double x1, double y1, double x2, double x2){
+        return Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2));
+    }
+
+    public double getDistance() {
         return distanceTraveled;
     }
 }
