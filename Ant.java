59664ec35da0bb848e19fd40241a9516839da925
Aiden Mizhen <amizhen30@stuy.edu>
2022-05-25 11:44:47 -0400

    Removed redundancy

--- Ant.java
+++ Ant.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class Ant {
     private double distanceTraveled;
-    public ArrayList<Node> nodesVisited;
+    public ArrayList<Node> visitedNodes;
     private ArrayList<Node> toBeVisited;
 
     public Ant() {
         distanceTraveled = 0;
-        nodesVisited = new ArrayList<Node>();
+        visitedNodes = new ArrayList<Node>();
         toBeVisited = new ArrayList<>();
     }
 
@@ -15,9 +15,6 @@ public class Ant {
         return Salesman.PHEROMONE_EVAPORATION_COEFFICIENT / getDistance();
     }
 
-    private double calc_dist(double x1, double y1, double x2, double y2){
-        return Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2));
-    }
 
     private void pickNextNode(){
     }
