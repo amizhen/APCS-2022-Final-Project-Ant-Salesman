c11ac1b575dc9ec586ea6aba1d6f5044eb8f9aad
Aiden Mizhen <amizhen30@stuy.edu>
2022-05-25 21:58:50 -0400

    Implamented pickNextNode

--- Ant.java
+++ Ant.java
@@ -4,6 +4,7 @@ public class Ant {
     private double distanceTraveled;
     public ArrayList<Node> visitedNodes;
     private ArrayList<Node> toBeVisited;
+    private Node current;
 
     public Ant() {
         distanceTraveled = 0;
@@ -17,6 +18,22 @@ public class Ant {
 
 
     private void pickNextNode(){
+        double total = 0;
+        double counter = 0;
+        double odds = Math.random();
+        for(Node adj : toBeVisited){
+            total += Salesman.pheromoneMap.get(Set.of(adj, current))+1;
+        }
+        Node adj;
+        for(int i = 0; i<toBeVisited.size(); i++){
+            adj = toBeVisited.get(i);
+            counter += Salesman.pheromoneMap.get(Set.of(adj, current))+1;
+            if(counter/total > odds){
+                toBeVisited.remove(i);
+                visitedNodes.add(adj);
+                return;
+            }
+        }
     }
 
     public void run() {
